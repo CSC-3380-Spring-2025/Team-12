805b1e166c94e59ef6f7dc8a934b321975539a17
TruongNguyen0607 <mikenguyen1236@gmail.com>
2025-03-06 00:19:04 -0600

    login form update

--- hacker-stories/src/components/LoginForm/LoginForm.jsx
+++ hacker-stories/src/components/LoginForm/LoginForm.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './loginForm.css';
-import { FaUser, FaLock } from "react-icons/fa";
+import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
 
 const LoginForm = () => {
+    const [action, setAction] = useState("");
+    const registerLink = () => {
+        setAction("active");
+    };
+    const loginLink = () => {
+        setAction("active");
+    };
     return (
-        <div className="wrapper">
-            <form action="">
+        <div className={"wrapper${action}"}>
+            <div className="form-box login">
+            <form action=""> 
                 <h1>Login</h1>
                 <div className="input-box">
                     <input type="text" placeholder="Username" required/>
@@ -21,9 +29,34 @@ const LoginForm = () => {
                 </div>
                 <button type="submit">Login</button>
                 <div className="register-link">
-                    <p>Don't have an account? <a href="#">Register</a></p>
+                    <p>Don't have an account? <a href="#" onClick={registerLink}>Register</a></p>
                 </div>
             </form>
+        </div>
+        <div className="form-box register">
+            <form action=""> 
+                <h1>Registation</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Username" required/>
+                    <FaUser className="icon"/>
+                </div>
+                <div className="input-box">
+                    <input type="email" placeholder="Email" required/>
+                    <FaEnvelope className="icon"/>
+                </div>
+                <div className="input-box">
+                    <input type="Password" placeholder="Password" required/>
+                    <FaLock className="icon"/>
+                </div>
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />I agree to the terms and conditions</label>
+                </div>
+                <button type="submit">Register</button>
+                <div className="register-link">
+                    <p>Already have an account? <a href="#" onClick={loginLink}>Login</a></p>
+                </div>
+            </form>
+        </div>
         </div>
     )
 }
