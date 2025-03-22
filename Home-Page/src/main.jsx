216f6abcff8ce2c1c6f4ee0297e7beddd343e81f
D <ljon188@lsu.edu>
2025-03-21 20:50:54 -0500

    Add blank pages
    Added 5 more pages, along with creating a page for the game, linking it to the play button, created the leaderboard page, linking it to the leaderboard button in the navbar
    Changed the names of some of the files and added all the web pages together into one folder so they are all together, but that did include moving some files around

--- Home-Page/src/main.jsx
+++ Home-Page/src/main.jsx
@@ -1,17 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // Correct import for TypeScript
+import { createBrowserRouter, RouterProvider } from 'react-router-dom' // Using react router
+import HomePage from "./pages/HomePage"; // Correct import for TypeScript
+import Play from "./pages/Play";
+import Leaderboard from "./pages/Leaderboard";
+import About from "./pages/About";
+import Page3 from "./pages/Page3";
+import Page2 from "./pages/Page2";
+
 import "./index.css";
 
 // Find the root element
 const rootElement = document.getElementById("root");
+
+// Create Client Browser Router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <div>404 Not Found</div>
+  },
+  {
+    path: '/play',
+    element: <Play />,
+  },
+  {
+    path: '/leaderboard',
+    element: <Leaderboard />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/page2',
+    element: <Page2 />,
+  },
+  {
+    path: '/Page3',
+    element: <Page3 />,
+  },
+]);
+
 
 // Ensure the root element exists before rendering
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   );
 } else {
